364d234f564884da4b25de6c51b6779190db0fad
Elliot Wasem <ewasem@stevens.edu>
2018-12-25 13:10:30 -0500

    better time before updates

--- christmas.c
+++ christmas.c
@@ -15,7 +15,7 @@ int main() {
   start_color();
   refresh();
   noecho();
-  timeout(100);
+  timeout(150);
   init_pair(1, COLOR_GREEN, COLOR_BLACK);
   init_pair(2, COLOR_RED, COLOR_BLACK);
   init_pair(3, COLOR_BLUE, COLOR_BLACK);
